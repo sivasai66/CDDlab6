edb488623a9bbd4f087ebf3eba39db8f6ce4549b
Siva sai Yarlagadda <67009737+sivasai66@users.noreply.github.com>
2021-09-14 12:19:39 +0530

    The Java File Commiting

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java"); 
+System.out.println("Continuous Integration Jenkins");  
+System.out.println("Maven Installation Done"); 
+    }  
+}  
