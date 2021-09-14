df1d4c0596c22e5df15ae21b69534eed1e6e57d7
Siva sai Yarlagadda <67009737+sivasai66@users.noreply.github.com>
2021-09-14 12:42:36 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Simple{  
+class helloworld{  
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
 System.out.println("Continuous Integration Jenkins");  
